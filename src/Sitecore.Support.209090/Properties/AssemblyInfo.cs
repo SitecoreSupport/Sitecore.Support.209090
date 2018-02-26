77cf2e16aeb1813116b0a80765cab8523cf3f8e6
AntonKechashin <atk@sitecore.net>
2018-02-26 15:56:11 +0200

    Added a processor injecting caching headers for SPEAK pages

---
+++ src/Sitecore.Support.209090/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.209090")]
+[assembly: AssemblyProduct("Sitecore.Support.209090")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.209090/Web/Pipelines/InitializeSpeakLayout/DisableCaching.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.Support.Web.Pipelines.InitializeSpeakLayout
+{
+  using System.Web;
+  using Sitecore.Configuration;
+  using Sitecore.Web.Pipelines.InitializeSpeakLayout;
+
+  public class DisableCaching
+  {
+    public void Process(InitializeSpeakLayoutArgs args)
+    {
+      if (Settings.DisableBrowserCaching)
+      {
+        HttpContext.Current.Response.Cache.SetNoStore();
+        HttpContext.Current.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+      }
+    }
+  }
+}
